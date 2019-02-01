a720213a4f025a5b9700d3664ed8242cfbbec3cf
juan.uzuriaga@gmail.com <juan.uzuriaga@gmail.com>
2019-02-01 18:04:44 -0500

    Changelog: - Se adicionó proyecto con las DTOS

--- BL/Class1.cs
+++ BL/Class1.cs
@@ -1,8 +1,17 @@
-﻿using System;
+﻿using DAL;
+using System;
 
 namespace BL
 {
     public class Class1
     {
+        private RestClient client;
+        public Class1()
+        {
+            this.client = new RestClient();
+        }
+
+
+
     }
 }
