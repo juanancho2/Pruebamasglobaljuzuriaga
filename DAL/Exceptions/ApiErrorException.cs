e864ee9305598e4f45b42b79367cbc2b3987cc74
juan.uzuriaga@gmail.com <juan.uzuriaga@gmail.com>
2019-02-04 13:01:11 -0500

    Se adiciona la carpeta DAL, que por error quedo fuera del proyecto

---
+++ DAL/Exceptions/ApiErrorException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Exceptions
+{
+    public class ApiErrorException : Exception
+    {
+        public ApiErrorException()
+        {
+        }
+
+        public ApiErrorException(string message)
+            : base(message)
+        {
+        }
+
+        public ApiErrorException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
---
+++ DAL/RestClient.cs
@@ -0,0 +1,55 @@
+﻿using DAL.Exceptions;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class RestClient
+    {
+        private HttpClient client;
+
+        public RestClient()
+        {
+            this.client = new HttpClient();
+        }
+
+        public void Init(string baseUri)
+        {
+            this.client.BaseAddress = new Uri(baseUri);
+            client.DefaultRequestHeaders.Accept.Clear();
+        }
+
+
+        public async Task<T> Get<T>(string path, Func<string, T> deserializeFn = null)
+            where T : class
+        {
+            HttpResponseMessage response = await client.GetAsync(path);
+            switch (response.StatusCode)
+            {
+
+                case System.Net.HttpStatusCode.NotFound:
+                    throw new NotFoundException();
+                case System.Net.HttpStatusCode.OK:
+
+                    if (deserializeFn == null)
+                    {
+                        return await response.Content.ReadAsAsync<T>();
+                    }
+
+                    var responseContent = await response.Content.ReadAsStringAsync();
+
+                    var myCustomResponse = await Task.Factory.StartNew(() => deserializeFn(responseContent));
+
+                    return myCustomResponse;
+                default:
+                    throw new ApiErrorException();
+            }
+        }
+
+    }
+}
